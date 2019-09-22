87bad7d3602a5a0e83bc1e1d17574d35122b4420
clarence pang <zwpang.2016@smu.edu.sg>
2019-09-23 05:10:42 +0800

    mvp 1.1

--- src/_constants/index.js
+++ src/_constants/index.js
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = ""
 export const API_BASE_URL = "http://localhost:8080";
 
-export const FIMATIC_ENGINE ="http://fimrec-env.djpzebgisx.us-east-2.elasticbeanstalk.com";
+export const FIMATIC_ENGINE ="http://dymanic-env.mbcatefpbp.us-east-2.elasticbeanstalk.com";
 
 export const ACCESS_TOKEN = 'accessToken';
 
@@ -15,3 +15,11 @@ export const GET_OVERVIEW ="/game/getOverview";
 
 //fimatic-engine
 export const RECOMMEND = "/recommend";
+
+export const NAV = "https://www.dbs.com.sg/personal/landing/nav/nav-hub.html";
+export const VICKERS = "https://www.dbsvonline.com/";
+export const HEALTH = "https://www.dbs.com.sg/personal/insurance/accident-health/personal-accident-plans/easy-protect";
+export const PERSONAL="https://www.dbs.com.sg/personal/insurance/protection/protection-plans/criticalselectcare";
+export const TRAVEL = "https://www.dbs.com.sg/personal/insurance/travel/bbm-travel-insurance";
+export const POSB ="https://www.posb.com.sg/personal/insurance/endowment/default.page";
+export const MANULIFE ="https://www.dbs.com.sg/personal/insurance/protection/protection-plans/manuprotect-decreasing-lite";
--- src/views/ProductRecommendation.js
+++ src/views/ProductRecommendation.js
@@ -1,43 +1,88 @@
 import React, {Component} from 'react';
 import {Services} from "../_services";
+import {calculateNetworth} from "../_utils";
+import Button from "reactstrap/es/Button";
+import {singleButtonStyle, singlePillButtonStyle} from "../css";
 
-class ProductRecommendation extends Component{
+class ProductRecommendation extends Component {
 
     constructor(props) {
         super(props);
-    }
-    componentDidMount() {
-        // JSON.parse(localStorage.getItem('user')).
-        let recomObject = {"ID":1,
-            "episode_no":2,
-            "newnetworth":58000,
-            "new_insurance_basic_health":0,
-            "new_insurance_critical_illness":0,
-            "new_insurance_home":0,
-            "new_insurance_travel":1,
-            "decisionscore_investment":0.74,
-            "decisionscore_bonds":0.1,
-            "decisionscore_savings":0.42}
-            Services.recommend(recomObject).then(result=>{
-                console.log(result);
-            }).catch(error =>{
-                console.log(error);
+        this.state = {
+            result: [
+                {
+                    zero: "",
+                    one: "",
+                    two: ""
+                }, {
+                    zero: "",
+                    one: "",
+                    two: "",
                 }
+            ]
 
-            );
+
+        }
+    }
+
+    componentDidMount() {
+        let state = this.props.location.state;
+        console.log(state);
+        // JSON.parse(localStorage.getItem('user')).
+        // JSON.parse(localStorage.getItem('user')).
+        let insurance = state.gameControls.insurance.value;
+        let recomObject = {
+            "ID": (JSON.parse(localStorage.getItem('user')))["clusterID"],
+            "episode_no": 2,
+            "newnetworth": calculateNetworth(state.gameControls),
+            "new_insurance_basic_health": insurance[0],
+            "new_insurance_critical_illness": insurance[1],
+            "new_insurance_home": insurance[2],
+            "new_insurance_travel": insurance[3],
+            "decisionscore_investment": state.ipoints,
+            "decisionscore_bonds": state.bpoints,
+            "decisionscore_savings": state.spoints
+        }
+
+        console.log(recomObject);
+        Services.recommend(recomObject).then(result => {
+            console.log(result);
+
+            this.setState({result: result})
+        }).catch(error => {
+                console.log(error);
+            }
+        );
 
     }
 
-    render(){
-        let endstate = this.props.location;
-        console.log(this.props);
-        console.log(this.props.location);
+    toPortfolio = (e) => {
+      this.props.history.push({pathname:"/portfolioallocation"})
+    };
+
+    render() {
+        // console.log(Object.values(this.state.result[0]));
+        console.log(this.state.result[0].zero);
         return (<div className="App">
             <header className="App-header">
                 <h1>BUY DBS PRODUCTS</h1>
+                <Button style={singleButtonStyle}
+                        href={this.state.result[1].zero}>
+                    {this.state.result[0].zero}
+                </Button>
+                <Button style={singleButtonStyle}
+                        href={this.state.result[1].one}>
+                    {this.state.result[0].one}
+                </Button>
+                <Button style={singleButtonStyle}
+                        href={this.state.result[1].two}>
+                    {this.state.result[0].two}
+                </Button>
+                <Button style={singlePillButtonStyle} onClick={()=> this.toPortfolio()}>Back to Portfolio Allocation</Button>
             </header>
         </div>)
     }
 
 }
+
 export default ProductRecommendation
