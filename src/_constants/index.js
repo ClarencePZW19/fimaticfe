612848dd17d1146e7c831b92083699b06858502c
clarence pang <zwpang.2016@smu.edu.sg>
2019-09-25 14:30:08 +0800

    mvp 1.1 fix

--- src/_constants/index.js
+++ src/_constants/index.js
@@ -2,6 +2,7 @@
 // export const API_BASE_URL = ""
 // export const API_BASE_URL = "http://localhost:8080";
 export const API_BASE_URL ="https://fimatic-be.herokuapp.com";
+
 export const FIMATIC_ENGINE ="http://dymanic-env.mbcatefpbp.us-east-2.elasticbeanstalk.com";
 
 export const ACCESS_TOKEN = 'accessToken';
